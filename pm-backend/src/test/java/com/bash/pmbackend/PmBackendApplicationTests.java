04317c4f068161be7f6dedd3efd25dfc63f0f50a
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-11-13 23:02:35 +0200

    setup postgres connection & h2 for tests

---
+++ pm-backend/src/test/java/com/bash/pmbackend/PmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.pmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

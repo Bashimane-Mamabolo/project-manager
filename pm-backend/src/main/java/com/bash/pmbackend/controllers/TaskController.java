77926791d27dcd6801e9ecc76d8a5907aefcd3bd
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-11-17 23:37:54 +0200

    impl get task of tasklist endpoint

--- pm-backend/src/main/java/com/bash/pmbackend/controllers/TaskController.java
+++ pm-backend/src/main/java/com/bash/pmbackend/controllers/TaskController.java
@@ -7,6 +7,7 @@ import com.bash.pmbackend.services.TaskService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -37,6 +38,15 @@ public class TaskController {
             @RequestBody TaskDto taskDto) {
         return taskMapper.toDTO(taskService.createTask(taskListId,
                 taskMapper.fromDTO(taskDto)));
+    }
+
+    @GetMapping(path = "/{task_id}")
+    public Optional<TaskDto> getTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @PathVariable("task_id") UUID taskId) {
+
+        return taskService.getTask(taskListId, taskId)
+                .map(taskMapper::toDTO);
     }
 
 }
--- pm-backend/src/main/java/com/bash/pmbackend/services/TaskService.java
+++ pm-backend/src/main/java/com/bash/pmbackend/services/TaskService.java
@@ -3,6 +3,7 @@ package com.bash.pmbackend.services;
 import com.bash.pmbackend.domain.entities.Task;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskService {
@@ -10,4 +11,5 @@ public interface TaskService {
     // Get the list of tasks that belong to a specific TaskList
     List<Task> listAllTasks(UUID taskListId);
     Task createTask(UUID taskListId, Task task); // Assign the Task to the tasklist of the taskListId
+    Optional<Task> getTask(UUID taskListId, UUID taskId);
 }
